83c03c1aeafebda95845bb9be66c1236bf68e4a3
AhmetOgulcanCankaya <ahmetogulcancankaya@gmail.com>
2023-12-13 21:51:29 +0300

    added uninstall template and install fix

--- uninstall.sh
+++ uninstall.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 
 #Start minikube
-minikube start --driver=docker
-minikube -p minikube docker-env
+minikube stop
 
 #Build images
 cd consumer-app && mvn install && docker build -t rabbit-consumer:0.0.1 -f Dockerfile . && cd ..
 cd publisher-app && mvn install && docker build -t rabbit-publisher:0.0.1 -f Dockerfile . && cd ..
 
 #Add secret
-kubectl apply -f rabbitmq-secrets.yaml
+kubectl delete -f rabbitmq-secrets.yaml
 
 #Install services and RabbitMQ
-helm install rabbitmq rabbitmq
-helm install consumer ./rabbit-consumer
-helm install publisher ./rabbit-publisher
+helm uninstall rabbitmq
+helm uninstall consumer
+helm uninstall publisher
 
 #Open internal port for testing
 kubectl port-forward svc/publisher-rabbit-publisher 9090:9090 &
