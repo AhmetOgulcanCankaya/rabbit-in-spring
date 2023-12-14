9a5b6a53cc344427c5d1aa11670ca03fe7392a2e
AhmetOgulcanCankaya <ahmetogulcancankaya@gmail.com>
2023-12-15 00:20:15 +0300

    dep management and  install.sh seperated and style

--- dependency-manager.sh
+++ dependency-manager.sh
@@ -1,3 +1,56 @@
 #!/bin/bash
 
-####TODO
+####Update the server and install base packages
+sudo apt-get update -y 
+sudo apt-get upgrade -y
+sudo apt install git apt-transport-https ca-certificates curl software-properties-common -y
+
+#add docker to sources
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+#add kubectl to sources
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.28/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.28/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
+#download minikube
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+
+#add helm to sources
+curl https://baltocdn.com/helm/signing.asc | gpg --dearmor | sudo tee /usr/share/keyrings/helm.gpg > /dev/null
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/helm.gpg] https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
+
+#update sources get ready for install
+sudo apt update -y
+
+#even after installation, terminal refresh is required in order to use docker
+sudo apt-get install docker-ce kubectl helm  -y
+sudo usermod -aG docker $USER 
+
+sudo install minikube-linux-amd64 /usr/local/bin/minikube
+
+#java 17 & mvn 3.9.5
+sudo apt install openjdk-17-jdk -y
+cd /tmp; wget https://dlcdn.apache.org/maven/maven-3/3.9.5/binaries/apache-maven-3.9.5-bin.tar.gz; 
+tar xf apache-maven-3.9.5-bin.tar.gz
+sudo mv apache-maven-3.9.5  /opt/maven
+sudo chown -R root:root /opt/maven
+
+#go back to starting directory
+cd -
+
+#link mvn exe under PATH and set ENV vars
+cat <<EOF >> mymavenvars.sh
+export JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64
+export M2_HOME=/opt/maven
+export MAVEN_HOME=/opt/maven
+export PATH=${M2_HOME}/bin:${PATH}
+EOF
+sudo cp mymavenvars.sh /etc/profile.d/
+rm mymavenvars.sh
+sudo ln -s /opt/maven/bin/mvn /usr/bin/mvn
+
+export JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64
+export M2_HOME=/opt/maven
+export MAVEN_HOME=/opt/maven
+export PATH=${M2_HOME}/bin:${PATH}
--- install.sh
+++ install.sh
@@ -1,77 +1,42 @@
 #!/bin/bash
 
-####INSTALL
-sudo apt-get update -y 
-sudo apt-get upgrade -y
-cd /opt
-apt install apt-transport-https ca-certificates curl software-properties-common -y
+#############################################################################
+##  git clone https://github.com/AhmetOgulcanCankaya/rabbit-in-spring.git  ##
+#############################################################################
 
-#docker
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-
-#kubectl
-curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.28/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
-echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.28/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
-
-#minikube
-curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
-
-sudo apt update -y
-sudo apt install docker-ce -y
-sudo usermod -aG docker $LOGNAME
-sudo apt-get update -y 
-sudo apt-get install -y kubectl
-install minikube-linux-amd64 /usr/local/bin/minikube
-minikube start
-
-#java 17 & mvn 3.9.5
-apt install openjdk-17-jdk -y
-cd /tmp; wget https://dlcdn.apache.org/maven/maven-3/3.9.5/binaries/apache-maven-3.9.5-bin.tar.gz
-tar xf apache-maven-3.9.5-bin.tar.gz
-sudo mv apache-maven-3.9.5  /opt/maven
-chown -R root:root /opt/maven
-
-#link mvn exe under PATH 
-cat <<EOF >> /etc/profile.d/mymavenvars.sh
-export JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64
-export M2_HOME=/opt/maven
-export MAVEN_HOME=/opt/maven
-export PATH=${M2_HOME}/bin:${PATH}
-EOF
-ln -s /opt/maven/bin/mvn /usr/bin/mvn
-
-#Install helm
-curl https://baltocdn.com/helm/signing.asc | gpg --dearmor | sudo tee /usr/share/keyrings/helm.gpg > /dev/null
-sudo apt-get install apt-transport-https --yes
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/helm.gpg] https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
-sudo apt-get update
-sudo apt-get install helm
-
-
-#Start minikube
+#Start minikube with docker driver
 minikube start --driver=docker
 minikube -p minikube docker-env
 minikube addons enable ingress
 
-#Install RabbitMQ
-helm install rabbitmq ./rabbitmq
+#Add secret
+kubectl apply -f rabbitmq-secrets.yaml
 
+#Install RabbitMQ and amqp ingress
+helm install rabbitmq ./rabbitmq
+kubectl apply -f amqp-ingress.yaml
+
+# Docker env eval to minikube is required for builded images to be recognized by minikube
+eval $(minikube -p minikube docker-env)
 
 #Build images
 cd consumer-app && mvn install && docker build -t rabbit-consumer:0.0.1 -f Dockerfile . && cd ..
 cd publisher-app && mvn install && docker build -t rabbit-publisher:0.0.1 -f Dockerfile . && cd ..
 
-#Add secret
-kubectl apply -f rabbitmq-secrets.yaml
-
 #Install services 
 helm install consumer ./rabbit-consumer
 helm install publisher ./rabbit-publisher
 
-#Open internal port for testing
-#kubectl port-forward svc/publisher-rabbit-publisher 9090:9090 &
+export NODE_PORT=$(kubectl get --namespace default -o jsonpath="{.spec.ports[0].nodePort}" services consumer-rabbit-consumer)
+export NODE_IP=$(kubectl get nodes --namespace default -o jsonpath="{.items[0].status.addresses[0].address}")
+echo $NODE_IP rabbitpub.local | sudo tee -a /etc/hosts
+echo $NODE_IP rabbitmq.local | sudo tee -a /etc/hosts
+echo $NODE_IP rabbitmq-amqp.local | sudo tee -a /etc/hosts
 
+
+echo "Publisher endpoint is http://rabbitpub.local/"
+echo "Rabbitmq management endpoint is http://rabbitmq.local/"
+echo "Rabbitmq amqp endpoint is http://rabbitmq-amqp.local/"
 
 
 
